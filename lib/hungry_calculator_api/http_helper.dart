cd235a18ff89a9ef4ddd3ae957f3d70987901437
0razzu <0razzu@protonmail.com>
2023-11-15 14:06:14 +0300

    GroupHttp.getById()

--- lib/hungry_calculator_api/http_helper.dart
+++ lib/hungry_calculator_api/http_helper.dart
@@ -8,7 +8,7 @@ import 'network.dart';
 class GroupHttp {
   String path = 'groups';
 
-  Future<Group> saveGroup(Group group) async {
+  Future<Group> save(Group group) async {
     final response =
         await Network(path: path).post(jsonEncode(CreateGroupRequest(
       title: group.title,
@@ -27,5 +27,47 @@ class GroupHttp {
     group.id = decodedResponse.id;
 
     return group;
+  }
+
+  Future<Group> getById(String id) async {
+    final response = await Network(path: '$path/$id').get();
+    final decodedResponse =
+        GetGroupResponse.fromJson(jsonDecode(response.body));
+
+    final participants = decodedResponse.participants
+        .map((participantDto) => GroupParticipant(
+              id: participantDto.id,
+              name: participantDto.name,
+            ))
+        .toList();
+
+    return Group(
+      id: id,
+      title: decodedResponse.title,
+      creator: GroupCreator(
+        id: decodedResponse.creator.id,
+        name: participants
+            .firstWhere(
+                (participant) => participant.id == decodedResponse.creator.id)
+            .name,
+        requisites: decodedResponse.creator.requisites,
+      ),
+      participants: decodedResponse.participants
+          .map((participantDto) => GroupParticipant(
+                id: participantDto.id,
+                name: participantDto.name,
+              ))
+          .toList(),
+      bill: decodedResponse.bill.positions
+          .map((positionDto) => BillPosition(
+                title: positionDto.title,
+                parts: Map.fromEntries(positionDto.payers.map((payer) =>
+                    MapEntry(
+                        participants.firstWhere(
+                            (participant) => participant.id == payer.id),
+                        payer.pricePerPart))),
+              ))
+          .toList(),
+    );
   }
 }
