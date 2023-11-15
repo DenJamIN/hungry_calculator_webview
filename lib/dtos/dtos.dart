1fd244a9cba8460e2f5929ce9debcad5fd8d239a
0razzu <0razzu@protonmail.com>
2023-11-15 15:58:45 +0300

    removed group getting

--- lib/dtos/dtos.dart
+++ lib/dtos/dtos.dart
@@ -7,10 +7,5 @@ export 'request/create_group_request_group_participant.dart';
 
 export 'response/create_group_participant_response.dart';
 export 'response/create_group_response.dart';
-export 'response/get_group_response.dart';
-export 'response/get_group_response_bill.dart';
-export 'response/get_group_response_bill_position.dart';
-export 'response/get_group_response_bill_position_payer.dart';
-export 'response/get_group_response_group_participant.dart';
 
 export 'group_creator_dto.dart';
